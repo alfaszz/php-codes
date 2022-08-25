ee586a3e3fc8de1b0f6e6c1db1aec96e03dbe185
alfaszz <110885328+alfaszz@users.noreply.github.com>
2022-08-25 19:38:33 +0530

    Create armstrong.php

---
+++ armstrong.php
@@ -0,0 +1,29 @@
+<html>
+
+<body>
+    <h2>Enter the Number</h2><br>
+    <form action="" method="post">
+        <input type="text" name="number" />
+        <input type="submit" />
+    </form>
+</body>
+
+</html>
+<?php
+if ($_POST) {
+    $number = $_POST['number'];
+    $temp = $number;
+    $sum = 0;
+    while ($temp != 0) {
+        $rem = $temp % 10;
+        $sum = $sum + ($rem * $rem * $rem);
+        $temp = $temp / 10;
+    }
+    if ($number == $sum)
+        echo $sum." is Armstrong Number";
+
+    else
+
+        echo $sum." is Not an Armstrong Number";
+}
+?>
